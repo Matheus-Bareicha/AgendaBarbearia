6455f647389ffaf6c47667738d32f06ac22573a7
Matheus Bareicha <matheusbareich@gmail.com>
2024-05-10 13:52:08 -0300

    finalização creserva

--- Sistema Leal/creserva.php
+++ Sistema Leal/creserva.php
@@ -1,4 +1,22 @@
 <?php
+require_once ("config.php");
+session_start();
+
+// Verificar se o usuário está logado
+if (!$_SESSION['LOGADO']) {
+    $msg = "Para acessar essa página é necessário realizar o Login";
+    header("Location: login_funcionario.php?m=$msg");
+    exit;
+}
+
+if (!$conexao) {
+    $msg = "Erro ao conectar no BD.";
+    header("Location: reservar_produtos.php?m=$msg");
+    exit;
+}
+
+$email = $_SESSION['LOGIN'];
+$msg = 'Produtos de ID :';
 
 
 $contador = 1;
@@ -13,13 +31,25 @@ foreach ($_POST as $chave => $valor) {
             // Verifica se a quantidade é diferente de zero
             if ($quantidade != 0) {
                 // Aqui você pode fazer o que quiser com o par de valores
-                echo "ID do Produto: $id_produto, Quantidade Selecionada: $quantidade <br>";
+                // echo "ID do Produto: $id_produto, Quantidade Selecionada: $quantidade <br>";
+                $sql = "INSERT INTO `reserva` (c_Email, Estoque_IDProduto, QTD, Estado) VALUES ('$email', '$id_produto', '$quantidade', 'P')";
+
+
+                if(mysqli_query($conexao, $sql)){
+                    $msg .= "'$id_produto', ";
+                }else{
+                    $msg = "Erro ao inserir produtos na reserva.";
+                    header("Location: reservar_produtos.php?m=$msg");
+                }
+
             }
         }
     }
     // Incrementa o contador
     $contador++;
 }
+$msg .= "inseridos com sucesso.";
+header("Location: reservar_produtos.php?m=$msg");
 
 
 
